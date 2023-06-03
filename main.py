5806fc142521962de66de44f388d4b0691097b1e
afa-spb <skorobey.spb@mail.ru>
2023-06-03 15:26:17 +0400

    added restart bot

--- main.py
+++ main.py
@@ -11,125 +11,121 @@ def _send_message(bot, chats_list, message):
             bot.send_message(chat, message, parse_mode="HTML", disable_web_page_preview=True)
 
 
-# Настройки -----------------------------------------------------------------------------------------------------------
-# забираем ключи из ini
-dict_with_keys = {}
-config = configparser.ConfigParser()
-config.read('config.ini')
-
-# telegram
-api = config.get('keys', 'api_key').strip()
-
-# bybit
-bybit_api_key = config.get('keys', 'bybit_api_key').strip()
-bybit_secret_key = config.get('keys', 'bybit_secret_key').strip()
-dict_with_keys['bybit'] = {'api_key': bybit_api_key, 'secret_key': bybit_secret_key}
-
-
-#  забираем настройки из ini
-# чаты
-chats = config.get('settings', 'chats').strip()
-chats_list = chats.strip('][').split(', ')
-
-# min profit
-min_profit_from_conf = float(config.get('settings', 'min_profit').strip())
-min_profit_usd_from_conf = float(config.get('settings', 'min_profit_usd').strip())
-max_invest_conf = float(config.get('settings', 'max_invest').strip())
-
-# валюты
-currencies = config.get('settings', 'currencies').strip()
-currencies = currencies.strip('][').split(', ')
-# удаляем повторяющиеся валюты
-new_list = []
-for currency in currencies:
-    if currency not in new_list:
-        new_list.append(currency)
-
-currencies = new_list
-
-
-# Логирование --------------------------------------------------------------------------------------------------------
-logging.basicConfig(
-    level=logging.INFO,  # Уровень логирования
-    format='%(asctime)s [%(levelname)s] %(message)s',  # Формат сообщений
-    handlers=[
-        logging.FileHandler("my_log.log"),  # Запись логов в файл
-    ]
-)
-
-# Программа -----------------------------------------------------------------------------------------------------------
-bot = telebot.TeleBot(api)  # запускаем бота
-
-# сообщение, что бот запущен
-_send_message(bot, chats_list, "Бот запущен")
-_send_message(bot, chats_list, f"Количество монет: {len(currencies)}")
-
-
-while True:
+def main_script(first_message):
     try:
-        start_time = time.time()  # Засекаем время начала выполнения кода
-        for currency in currencies:  # в отношении каждой валюты
-            all_orders = get_orders_from_exchanges(currency)  # получаем все ордера
-            if len(all_orders) > 0:  # если ордеров больше 0
-                previous_message = ''
-                for order in all_orders:  # в отношении каждого ордера
-                    order_buy = order['order_buy'] # ордер на покупку
-                    orders_sell = order['orders_sell']  # ордера на продажу
-                    need_spent = order['need_spent']  # надо потратить
-                    need_bought = order['need_bought'] # надо купить монет
-                    profit = order['margin'] # профит в процентах
-                    profit_in_dol = order['margin_in_dol']  # профит в долларах
+        # Настройки --------------------------------------------------------------------------------------------------------
+        # забираем ключи из ini
+        dict_with_keys = {}
+        config = configparser.ConfigParser()
+        config.read('config.ini')
 
-                    # если профит больше минимального и надо потратить меньше максимального
-                    if profit >= min_profit_from_conf \
-                            and profit_in_dol >= min_profit_usd_from_conf \
-                            and need_spent <= max_invest_conf:
+        # telegram
+        api = config.get('keys', 'api_key').strip()
 
-                        # формируем спсок из всех ордеров на проаджу
-                        text_orders_sell = ''
-                        for order_sell in orders_sell:  # в отношении каждого ордера на продажу
-                            string = f'Цена: {order_sell[2]}, кол-во: {order_sell[3]}\n'
-                            text_orders_sell += string
-
-                        # формируем сообщение
-                        message = f"<b>Валютная пара:</b> {currency}/usdt\n\n" \
-                                  f"<b>Покупка:</b>\n" \
-                                  f"Биржа: <a href='{orders_sell[0][1]}'>{orders_sell[0][0]}</a>\n" \
-                                  f"{text_orders_sell}\n" \
-                                  f"<b>Надо:</b>\n" \
-                                  f"потратить: {round(need_spent, 2)}$\n" \
-                                  f"что бы купить: {round(need_bought, 4)} монет\n\n" \
-                                  f"<b>Продажа:</b>\n" \
-                                  f"Биржа: <a href='{order_buy[1]}'>{order_buy[0]}</a>\n" \
-                                  f"Цена: {order_buy[3]}\n" \
-                                  f"Кол-во: {order_buy[4]}\n\n" \
-                                  f"<b>Тогда прибыль:</b>\n" \
-                                  f"В %: {profit}%\n"\
-                                  f"В $: {profit_in_dol}$\n"
-
-                        if message != previous_message:  # если сообщение не равно предыдущему
-                            _send_message(bot, chats_list, message)
-                        previous_message = message  # сохраняем сообщение как предудщее, для проверки, что бы они не повторялись
-
-        end_time = time.time()  # Засекаем время окончания выполнения кода
-        elapsed_time = end_time - start_time  # Вычисляем затраченное время
-        elapsed_time = round(elapsed_time, 2)
-
-        text = f"Полный круг. Время выполнения кода: {elapsed_time} секунд"
-        for chat in chats_list:
-            if len(chat) > 0:
-                logging.info(text)
-                bot.send_message(chat, text, parse_mode="HTML")
-
-    except Exception as e:
-        logging.error(e)
-        _send_message(bot, chats_list, "Упс. Какая-то ошибочка на самом верхнем уровне")
+        # bybit
+        bybit_api_key = config.get('keys', 'bybit_api_key').strip()
+        bybit_secret_key = config.get('keys', 'bybit_secret_key').strip()
+        dict_with_keys['bybit'] = {'api_key': bybit_api_key, 'secret_key': bybit_secret_key}
 
 
-def start_bot():
-    pause_sec = 30
-    try:
-        bot.infinity_polling()
+        #  забираем настройки из ini
+        # чаты
+        chats = config.get('settings', 'chats').strip()
+        chats_list = chats.strip('][').split(', ')
+
+        # min profit
+        min_profit_from_conf = float(config.get('settings', 'min_profit').strip())
+        min_profit_usd_from_conf = float(config.get('settings', 'min_profit_usd').strip())
+        max_invest_conf = float(config.get('settings', 'max_invest').strip())
+
+        # валюты
+        currencies = config.get('settings', 'currencies').strip()
+        currencies = currencies.strip('][').split(', ')
+        # удаляем повторяющиеся валюты
+        new_list = []
+        for currency in currencies:
+            if currency not in new_list:
+                new_list.append(currency)
+
+        currencies = new_list
+
+        # Логирование ------------------------------------------------------------------------------------------------------
+        logging.basicConfig(
+            level=logging.INFO,  # Уровень логирования
+            format='%(asctime)s [%(levelname)s] %(message)s',  # Формат сообщений
+            handlers=[
+                logging.FileHandler("my_log.log"),  # Запись логов в файл
+            ]
+        )
+
+        # Программа --------------------------------------------------------------------------------------------------------
+        bot = telebot.TeleBot(api)  # запускаем бота
+
+        # сообщение, что бот запущен
+        _send_message(bot, chats_list, f"Бот запущен, {first_message}")
+        _send_message(bot, chats_list, f"Количество монет: {len(currencies)}")
+
+
+
+        while True:
+            try:
+                start_time = time.time()  # Засекаем время начала выполнения кода
+                for currency in currencies:  # в отношении каждой валюты
+                    all_orders = get_orders_from_exchanges(currency)  # получаем все ордера
+                    if len(all_orders) > 0:  # если ордеров больше 0
+                        previous_message = ''
+                        for order in all_orders:  # в отношении каждого ордера
+                            order_buy = order['order_buy'] # ордер на покупку
+                            orders_sell = order['orders_sell']  # ордера на продажу
+                            need_spent = order['need_spent']  # надо потратить
+                            need_bought = order['need_bought'] # надо купить монет
+                            profit = order['margin'] # профит в процентах
+                            profit_in_dol = order['margin_in_dol']  # профит в долларах
+
+                            # если профит больше минимального и надо потратить меньше максимального
+                            if profit >= min_profit_from_conf \
+                                    and profit_in_dol >= min_profit_usd_from_conf \
+                                    and need_spent <= max_invest_conf:
+
+                                # формируем спсок из всех ордеров на проаджу
+                                text_orders_sell = ''
+                                for order_sell in orders_sell:  # в отношении каждого ордера на продажу
+                                    string = f'Цена: {order_sell[2]}, кол-во: {order_sell[3]}\n'
+                                    text_orders_sell += string
+
+                                # формируем сообщение
+                                message = f"<b>Валютная пара:</b> {currency}/usdt\n\n" \
+                                          f"<b>Покупка:</b>\n" \
+                                          f"Биржа: <a href='{orders_sell[0][1]}'>{orders_sell[0][0]}</a>\n" \
+                                          f"{text_orders_sell}\n" \
+                                          f"<b>Надо:</b>\n" \
+                                          f"потратить: {round(need_spent, 2)}$\n" \
+                                          f"что бы купить: {round(need_bought, 4)} монет\n\n" \
+                                          f"<b>Продажа:</b>\n" \
+                                          f"Биржа: <a href='{order_buy[1]}'>{order_buy[0]}</a>\n" \
+                                          f"Цена: {order_buy[3]}\n" \
+                                          f"Кол-во: {order_buy[4]}\n\n" \
+                                          f"<b>Тогда прибыль:</b>\n" \
+                                          f"В %: {profit}%\n"\
+                                          f"В $: {profit_in_dol}$\n"
+
+                                if message != previous_message:  # если сообщение не равно предыдущему
+                                    _send_message(bot, chats_list, message)
+                                previous_message = message  # сохраняем сообщение как предудщее, для проверки, что бы они не повторялись
+
+                end_time = time.time()  # Засекаем время окончания выполнения кода
+                elapsed_time = end_time - start_time  # Вычисляем затраченное время
+                elapsed_time = round(elapsed_time, 2)
+
+                text = f"Полный круг. Время выполнения кода: {elapsed_time} секунд"
+                for chat in chats_list:
+                    if len(chat) > 0:
+                        logging.info(text)
+                        bot.send_message(chat, text, parse_mode="HTML")
+
+            except Exception as e:
+                logging.error(e)
+                _send_message(bot, chats_list, "Упс. Какая-то ошибочка на самом верхнем уровне")
 
     except Exception as e:
         text_for_log = f"-------------------------------------------------------------" \
@@ -137,17 +133,15 @@ def start_bot():
                        f"Ошибка: {e}" \
                        f"-------------------------------------------------------------"
         logging.error(text_for_log)
+        time.sleep(30)
+        main_script(f'перезапуск после ошибки: {str(e)}')
 
-        text_for_msg = "Бот упал, ошибку смотри в логах"
-        _send_message(bot, chats_list, text_for_msg)
 
-        time = 0
-        while time < pause_sec:
-            time += 1
-            time.sleep(1)
-            print(f"До перезапуска бота осталось {pause_sec - time} секунд")
+main_script('первый запуск')
 
-        start_bot()
 
-start_bot()
+
+
+#bot.infinity_polling()
+
 
